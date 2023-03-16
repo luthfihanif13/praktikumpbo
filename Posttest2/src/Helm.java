08f23790cb072b0f7aeaa266f91386e6bc17dc79
luthfihanif13 <67750065+luthfihanif13@users.noreply.github.com>
2023-03-16 18:04:34 +0800

    Add files via upload

---
+++ Posttest2/src/Helm.java
@@ -0,0 +1,38 @@
+class Helm{
+    private String nama;
+    private String merk;
+    private int harga;
+
+    public Helm(String nama, String merk, int harga) {
+        this.nama = nama;
+        this.merk = merk;
+        this.harga = harga;
+    }
+
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getMerk() {
+        return merk;
+    }
+    
+    public int getHarga() {
+        return harga;
+    }
+
+    public void SetNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void SetMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public void SetHarga(int harga) {
+        this.harga = harga;
+    }
+}
+
+
